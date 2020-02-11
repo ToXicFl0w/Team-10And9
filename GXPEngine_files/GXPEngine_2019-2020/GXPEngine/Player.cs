7897e8f444068056baf2cd727c0e14255a566f09
ToXicFl0w <60521974+ToXicFl0w@users.noreply.github.com>
2020-02-11 15:16:06 +0100

    Added GXP-Engine

---
+++ GXPEngine_files/GXPEngine_2019-2020/GXPEngine/Player.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using GXPEngine;
+public class Player : Sprite
+{
+    private float _speed = 0.4f;
+    private float _gravity = 1.0f;
+    public static int gameRotation= 1;
+
+
+    //------------------------------------------------------------
+    //                      Player()
+    //------------------------------------------------------------
+    public Player() : base("Assets/Chonky.png")
+    {
+        SetOrigin(width / 2, height);                                           // Set center of the sprite.
+    }
+
+    //------------------------------------------------------------
+    //                      movementInput()
+    //------------------------------------------------------------
+    void MovementInput()
+    {
+        if(Input.GetKey(Key.A))
+        {
+            Move(-(Time.deltaTime * _speed), 0f);
+        }
+        if (Input.GetKey(Key.D))
+        {
+            Move((Time.deltaTime * _speed), 0f);
+        }
+        if (Input.GetKey(Key.Q))
+        {
+            gameRotation -= 1;
+        }
+        if(Input.GetKey(Key.E))
+        {
+            gameRotation += 1;
+        }
+    }
+
+    void GravityEffect()
+    {
+        Console.WriteLine(parent.rotation);
+        MoveUntilCollision(0, _gravity);
+    }
+
+    void OutOfScreen()
+    {
+        if(this.y >= 700)
+        {
+            this.y = 600;
+        }
+    }
+
+    void Update()
+    {
+        GravityEffect();
+        MovementInput();
+        OutOfScreen();
+    }
+}
